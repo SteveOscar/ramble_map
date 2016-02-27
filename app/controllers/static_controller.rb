5cc7d75a61c8058ac041fe76d84d5ae3098cbe75
Steven Olson <steveoscaro@gmail.com>
2016-02-27 09:03:49 -0700

    map renders correctly on page

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,44 @@
+require 'json'
+require 'csv'
+
+class StaticController < ApplicationController
+  def home
+    @latest = {}
+    rates = Stats.new.get_data["rates"]
+    Country.all.each do |country|
+      @latest[country.map_code] = rates[country.currency.code].to_s unless rates[country.currency.code].nil?
+    end
+    @stats = compare(@latest)
+    gon.stats = @stats
+  end
+
+  private
+
+  def compare(latest)
+    old = HistoricalData.first[:data]
+    change = {}
+    latest.each do |rate|
+      change[rate[0]] = ((rate[1].to_f - old[rate[0]].to_f) / rate[1].to_f).to_s[0..4]
+    end
+    @percent = change
+    gon.percent = @percent
+    set_range(change)
+  end
+
+  def set_range(change)
+    data = {}
+    change.each do |rate|
+      diff = rate[1].to_f
+      data[rate[0]] = 1 if diff < -0.1
+      data[rate[0]] = 2 if diff >= -0.1 && diff < 0
+      data[rate[0]] = 3 if diff > 0 && diff < 0.01
+      data[rate[0]] = 4 if diff >= 0.01 && diff < 0.02
+      data[rate[0]] = 5 if diff >= 0.02 && diff < 0.03
+      data[rate[0]] = 1 if diff >= 0.03
+    end
+    data
+  end
+
+
+
+end
